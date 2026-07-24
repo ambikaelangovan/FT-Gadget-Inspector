cd8cbc89b7b0e7dcab9f911a42cf1efe95b90781
ambikaelangovan <aelangovan@wm.edu>
2026-07-24 15:06:07 -0400

    changed from ipynb to py

---
+++ scaffolding/circuits.py
@@ -0,0 +1,13 @@
+class CircuitCreation:
+
+    def baseline_circuit(self):
+        """ standard ancilla stabilizer measurement circuit"""
+        pass
+
+    def flag_circuits(self):
+        """flag-qubit stabilizer measurement circuit"""
+        pass
+
+    def get_circuit(self, circuit_type):
+        """either baseline or flag qubit"""
+        pass
---
+++ scaffolding/faults.py
@@ -0,0 +1,16 @@
+class FaultInjection:
+
+    def inject_faults(self, circuit, location, pauli_fault):
+        """injects the X, Y, or Z pauli faults
+        random????
+        """
+        pass
+
+    def fault_locations(self, circuit):
+        """returns possible locations the fualt is in the circuit"""
+        pass
+
+    def all_faults(self, circuit):
+        """each combination of the faults and where they are """
+        pass
+
