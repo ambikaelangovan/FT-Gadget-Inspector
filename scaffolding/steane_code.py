d52d8c988f4958b09ce394c77c8edd81fb34c695
ambikaelangovan <aelangovan@wm.edu>
2026-07-28 15:14:13 -0400

    Update steane_code.py

--- scaffolding/steane_code.py
+++ scaffolding/steane_code.py
@@ -1,14 +1,50 @@
 class Steane:
 
-    def stabilizers(self):
-        """returns the steane stabilizer generator"""
-        pass
+    def __init__(self):
+        "initializing the steane stabilizer generators"
+        self.stabilizers = {
+            "X" : [[3,4,5,6]]
+        }
 
-    def decode(syndrome):
-        """takes in the measured syndrome returns which corrections needs to be applied to which qubit"""
-        pass
+
+    def stabilizers(self):
+        "returns the steane stabilizer generator, form of a dictionary"
+        return self.stabilizers
+
+    def decode(self, syndrome):
+        "takes in the measured syndrome returns which corrections needs to be applied to which qubit"\
+
+        if syndrome == 0:
+            return {
+                "detected" : False,
+                "corrections" : None,
+                "message" : "No error detected"
+            }
+
+        elif syndrome == 1:
+            return {
+                "detected" : True,
+                "corrections" : "Unknown",
+                "message" : "Error detected"
+            }
+
+        else:
+            raise ValueError("Invalid syndrome: must be 0 or 1")
+
 
     def correctable(self, error):
-        """is the error fixable by steane code or not?"""
-        pass
+        "is the error fixable by steane code or not?"
+
+        if error is None:
+            return True
+
+        if not isinstance(error, dict):
+            raise TypeError("Invalid syndrome: must be a dictionary")
+
+        qubit = error.get("qubit")
+
+        if qubit is not None and 0<=qubit<=6 :
+            return True
+
+        return False
     
